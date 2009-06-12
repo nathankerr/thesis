51f5736574932dcedb56d2f947387482b8347948
Nathan Kerr <alaster@MediaMac.local>
2009-06-12 11:49:37 -0700

    Added thesis outline

---
+++ doc/NathanKerr_thesis/thesis/outline.mm
@@ -0,0 +1,105 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1244761479812" ID="ID_1607972445" MODIFIED="1244761495796" TEXT="Nathan&apos;s Thesis">
+<node CREATED="1244761545203" ID="ID_919224041" MODIFIED="1244761560953" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Abstract
+    </p>
+    <p>
+      <font size="2">150 Words</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1244761499453" ID="ID_1059610229" MODIFIED="1244762159812" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Introduction
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1244762137906" ID="ID_1040520283" MODIFIED="1244762140218" TEXT="Background"/>
+<node CREATED="1244762149484" ID="ID_985555486" MODIFIED="1244762151437" TEXT="Context"/>
+<node CREATED="1244762143734" ID="ID_1148785423" MODIFIED="1244762148015" TEXT="Related Work"/>
+</node>
+<node CREATED="1244761505531" ID="ID_166006262" MODIFIED="1244762213984" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Thesis Statement
+    </p>
+    <p>
+      <font size="2">(35 words)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1244761708062" ID="ID_1122243053" MODIFIED="1244762308312" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Research
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Body of work
+    </p>
+    <p>
+      <font size="2">(Obama)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1244761602171" ID="ID_1603951138" MODIFIED="1244761605468" TEXT="Operation Set">
+<node CREATED="1244762083234" ID="ID_603824357" MODIFIED="1244762194546" TEXT="Data access"/>
+<node CREATED="1244762089906" ID="ID_983085292" MODIFIED="1244762100906" TEXT="OGC standard requirements"/>
+</node>
+<node CREATED="1244761630406" ID="ID_903259215" MODIFIED="1244761634640" TEXT="Harware Setup"/>
+<node CREATED="1244761783984" ID="ID_853725809" MODIFIED="1244761787078" TEXT="Implementations">
+<node CREATED="1244761593562" ID="ID_457305762" MODIFIED="1244761600171" TEXT="PostGIS"/>
+<node CREATED="1244761639062" ID="ID_390381553" MODIFIED="1244761644234" TEXT="Hadoop Prototype"/>
+<node CREATED="1244761649750" ID="ID_1109229689" MODIFIED="1244761656531" TEXT="MPI Prototype"/>
+</node>
+</node>
+<node CREATED="1244761754765" ID="ID_403262888" MODIFIED="1244761759750" POSITION="right" TEXT="Results">
+<node CREATED="1244761831218" ID="ID_604452279" MODIFIED="1244761836265" TEXT="Performance Analysis"/>
+<node CREATED="1244761837484" ID="ID_427323911" MODIFIED="1244761842546" TEXT="Comparisons"/>
+</node>
+<node CREATED="1244761871718" ID="ID_1110813491" MODIFIED="1244761875734" POSITION="right" TEXT="Recomendation">
+<node CREATED="1244761911828" ID="ID_753877231" MODIFIED="1244761921359" TEXT="Synthesis"/>
+<node CREATED="1244761922968" ID="ID_1179952677" MODIFIED="1244762006125" TEXT="Analysis of problem types that are good/bad for this approach"/>
+</node>
+<node CREATED="1244761662718" ID="ID_1004394493" MODIFIED="1244761682734" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Conclusion and Outlook
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
