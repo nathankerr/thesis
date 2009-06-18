e190512d0cef3af0b054b4d7bbd86bd75fb37737
Nathan Kerr <alaster@asu.edu>
2009-06-18 14:14:56 -0700

    partial record completion now works

--- src/pgis.c
+++ src/pgis.c
@@ -1,7 +1,7 @@
 #include "clustergis.h"
 
 int main(int argc, char** argv) {
-	clusterGIS* head;
+	clusterGIS_dataset dataset;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s filename\n", argv[0]);
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 
 	clusterGIS_Init(&argc, &argv);
 
-	clusterGIS_Load_data_distributed(argv[1], head);
+	clusterGIS_Load_data_distributed(argv[1], &dataset);
 
 	clusterGIS_Finalize();
 	return 0;
